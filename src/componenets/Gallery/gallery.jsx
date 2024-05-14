fbfad6b920c9704f0a42d78642585e2f850682b4
Tabsir <tabsirconnects@gmail.com>
2024-05-14 11:30:34 +0600

    commit

--- src/componenets/Gallery/gallery.jsx
+++ src/componenets/Gallery/gallery.jsx
@@ -3,9 +3,9 @@ const Gallery = () => {
 
     return (
         <section className=" flex justify-evenly items-center w-[90%] mx-auto my-8 gap-4">
-            <img src="pic1.png" className=" w-[44%] h-[28vw]"/>
-            <img src="pic2.png" className=" w-[28%] h-[28vw]"/>
-            <img src="pic3.png" className=" w-[28%] h-[28vw]"/>
+            <img src="pic1.png" className=" w-[44%] h-[29vw]"/>
+            <img src="pic2.png" className=" w-[28%] h-[29vw]"/>
+            <img src="pic3.png" className=" w-[28%] h-[29vw]"/>
         </section>
     )
 }
